795feb8ea5c569988722f215f957872fda157711
Yuurij <77763443+Yuurij-dot@users.noreply.github.com>
2024-03-13 22:31:27 +0000

    add ´database.js´ and ´.env´ files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function curso(request, response) {
-    response.status(200).json({chave: 'curso.dev'})
+import database from "../../../../infra/database.js";
 
+async function curso(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  response.status(200).json({ chave: "curso.dev" });
 }
-export default curso
+export default curso;
